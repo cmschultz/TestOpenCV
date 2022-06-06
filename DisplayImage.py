b37be2eb2a73ffaede0968398db9d3d729fdc9ef
cmschultz <caioschultz@unifei.edu.br>
2022-06-06 00:40:01 -0300

    vai

--- DisplayImage.py
+++ DisplayImage.py
@@ -5,7 +5,6 @@ img = cv2.imread("./imagens/kuririn.jpg", 1)     # -1 == cv2.IMREAD_COLOR
 img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)     # altera o tamanho para a metade
 img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-cv2.imwrite('newKuririn.jpg', img)
 
 cv2.imshow("kuririn", img)                       # Label/variável
 cv2.waitKey(0)                                   # Espera o user apertar qq botão (key), se colocasse um numero 
